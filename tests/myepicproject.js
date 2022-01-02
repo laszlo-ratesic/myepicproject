d26da5b5d6ac09fb49de97294e7ccfa18efd0732
laszlo-ratesic <keenanrc@hotmail.com>
2022-01-02 13:44:39 -0600

    first commit

--- tests/myepicproject.js
+++ tests/myepicproject.js
@@ -1,13 +1,45 @@
 const anchor = require('@project-serum/anchor');
 
+const { SystemProgram } = anchor.web3;
+
 const main = async() => {
   console.log("🚀 Starting test...")
 
+  // Create and set the provider
+  const provider = anchor.Provider.env();
   anchor.setProvider(anchor.Provider.env());
+
   const program = anchor.workspace.Myepicproject;
-  const tx = await program.rpc.startStuffOff();
+
+  // Create a n account keypair for our program
+  const baseAccount = anchor.web3.Keypair.generate();
+
+  // Call start_stuff_off, pass it the parameters it needs
+  const tx = await program.rpc.startStuffOff({
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+      user: provider.wallet.publicKey,
+      systemProgram: SystemProgram.programId,
+    },
+    signers: [baseAccount],
+  });
 
   console.log("📝 Your transaction signature", tx);
+
+  // Fetch data from the account
+  let account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log('👀 GIF Count', account.totalGifs.toString())
+
+  // Call add_gif!
+  await program.rpc.addGif({
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+    },
+  });
+
+  // Get the account again to see what changed
+  account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log('👀 GIF Count', account.totalGifs.toString())
 }
 
 const runMain = async () => {
@@ -15,7 +47,7 @@ const runMain = async () => {
     await main();
     process.exit(0);
   } catch (error) {
-    console.log(error(error));
+    console.error(error);
     process.exit(1);
   }
 };
